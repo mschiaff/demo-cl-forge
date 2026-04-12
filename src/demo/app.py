f352b6866e2953e442bd92953c6dc3e52336385d
Matías Schiaffino Tyrer <matias.scht@gmail.com>
2026-04-11 22:16:09 -0400

    refactor: update navigation structure and page titles

--- src/demo/app.py
+++ src/demo/app.py
@@ -3,9 +3,14 @@ from __future__ import annotations
 import streamlit as st
 
 home_page = st.Page("pages/home.py", title="Inicio", icon="🏠")
-rut_page = st.Page("pages/rut.py", title="Cálculo y Validación de RUT", icon="🔍")
-ppu_page = st.Page("pages/ppu.py", title="Cálculo y Validación de Patente", icon="🔍")
+rut_page = st.Page("pages/rut.py", title="Rut", icon=":material/person:")
+ppu_page = st.Page("pages/ppu.py", title="Patente", icon=":material/airport_shuttle:")
 
-pg = st.navigation([home_page, rut_page, ppu_page])
+pg = st.navigation(
+    {
+        "Bienvenido": [home_page],
+        "Validación": [rut_page, ppu_page],
+    }
+)
 
 pg.run()
