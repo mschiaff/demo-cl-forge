3ec458bcf6f3267d2b73e1f10f5e4fe00e1cb261
Matías Schiaffino Tyrer <matias.scht@gmail.com>
2026-04-12 02:54:40 -0400

    feat(cmf): add sidebar API key input and data retrieval

--- src/demo/pages/cmf.py
+++ src/demo/pages/cmf.py
@@ -1,8 +1,61 @@
 from __future__ import annotations
 
 import streamlit as st
+from cl_forge.cmf import IpcEndpoint
+from streamlit import session_state as state
+from streamlit.column_config import DateColumn, NumberColumn
 
 st.set_page_config(
     page_title="Clientes API | CMF",
     page_icon="🏦"
 )
+
+
+if "cmf_api_key" not in state:
+    state.cmf_api_key: str | None = None # type: ignore
+
+
+with st.sidebar:
+    st.text_input(
+        label="API Key",
+        placeholder="Ingrese su API Key de la CMF",
+        type="password",
+        help=(
+            "⚠️ No es almacenada ni compartida. Solo "
+            "se utiliza para esta sesión, y se borra "
+            "al cerrar la aplicación o al recargar "
+            "la página."
+        ),
+        key="cmf_api_key",
+    )
+
+
+if state.cmf_api_key and st.button("Obtener"):
+    ipc = IpcEndpoint(state.cmf_api_key)
+    obj = ipc.current()
+    st.dataframe(
+        data={
+            "Fecha": [obj.date],
+            "Valor": [obj.value],
+        },
+        column_config={
+            "Fecha": DateColumn("Fecha"),
+            "Valor": NumberColumn("Valor", format="percent"),
+        }
+    )
+
+if state.cmf_api_key and st.button("Obtener Año"):
+    ipc = IpcEndpoint(state.cmf_api_key)
+    obj = ipc.year(2025)
+    st.dataframe(
+        data={
+            "Fecha": [v.date for v in obj],
+            "Valor": [v.value for v in obj],
+        },
+        column_config={
+            "Fecha": DateColumn("Fecha"),
+            "Valor": NumberColumn("Valor", format="percent"),
+        }
+    )
+
+st.write(state)
